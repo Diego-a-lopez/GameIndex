2037b410b4b00aa9a7eede1e500cb89f739ffd7e
Diego-a-lopez <71869193+Diego-a-lopez@users.noreply.github.com>
2023-10-06 19:51:12 +0200

    spider español

--- HelpCode (no forma parte de la práctica)/Test.py
+++ HelpCode (no forma parte de la práctica)/Test.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 url = 'https://store.steampowered.com/app/2195250/EA_SPORTS_FC_24/'
+url = 'https://store.steampowered.com/app/1637320?snr=1_4_4__40_2'
 headers = {'Accept-Language': 'es-ES,es;q=0.9'}
 page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -23,9 +24,15 @@ if div_element:
 	genero_links = genero_span.find_all('a')
 	genero = [link.get_text(strip=True) for link in genero_links]
 			
-	desarrollador = div_element.find('b', string='Desarrollador:').find_next('a').get_text(strip=True)
-	editor = div_element.find('b', string='Editor:').find_next('a').get_text(strip=True)
-	franquicia = div_element.find('b', string='Franquicia:').find_next('a').get_text(strip=True)
+	desarrollador_elements = div_element.find_all('b', string='Desarrollador:')
+	desarrolladores = [element.find_next('a').get_text(strip=True) for element in desarrollador_elements]
+	
+	editor_elements = div_element.find_all('b', string='Editor:')
+	editores = [element.find_next('a').get_text(strip=True) for element in editor_elements]
+
+	franquicia_elements = div_element.find_all('b', string='Franquicia:')
+	franquicias = [element.find_next('a').get_text(strip=True) for element in franquicia_elements]
+
 
 	fecha_lanzamiento_str = div_element.find('b', string='Fecha de lanzamiento:').next_sibling.strip()
 	
@@ -34,24 +41,32 @@ if div_element:
 	price_element = soup.find('div', class_='game_purchase_price')
 	
 	discount_element = soup.find('div', class_='discount_original_price')
+	
+	#Game Image
+	
+	imagen_cabecera = soup.find('img', class_='game_header_image_full')['src']
+	descripcion = soup.find('div', class_="game_description_snippet").get_text(strip=True)
+	images_list = soup.find('div', id= "highlight_strip_scroll").find_all('img')
+	
+	lista_imagenes = []
+	
+	for element in images_list:
+		lista_imagenes.append(element['src'])
 			
 	if price_element:
-		price = price_element.get_text(strip=True)
+		price = float(price_element.get_text(strip=True).replace(",", ".").replace("€", ""))
 	elif discount_element:
-		price = discount_element.get_text(strip=True)
+		price = float(discount_element.get_text(strip=True).replace(",", ".").replace("€", ""))
 	else:
 		price = "Precio no disponible"
-			
+
 	# Print the formatted output
 	print(f"Título: {titulo}")
-
+	print(f"Descripción: {descripcion}")
 	print(f"Género: {genero}")
-	print(f"Desarrollador: {desarrollador}")
-	print(f"Editor: {editor}")
-	print(f"Franquicia: {franquicia}")
+	print(f"Desarrollador: {desarrolladores}")
+	print(f"Editor: {editores}")
+	print(f"Franquicia: {franquicias}")
 	print(f"Fecha de lanzamiento: {fecha_lanzamiento}")
 	print(f"Precio: {price}")
-
-#print(page.text)
-
 
--- SteamScrap/SteamScrap/spiders/crawling_spider.py
+++ SteamScrap/SteamScrap/spiders/crawling_spider.py
@@ -1,21 +1,104 @@
+import scrapy
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from scrapy.http import Request
 
 """
 
 """
 class TestCrawlingSpider(CrawlSpider):
-	name = ">Steamcrawler"
-	allowed_domains = ["steampowered.com"]
+	name = "Steamcrawler"
+	allowed_domains = ["steampowered.com/"]
 	start_urls = ["https://store.steampowered.com/"]
 	
 	rules = (
-		Rule(LinkExtractor(allow="/app",), callback="parse_item")
+		Rule(LinkExtractor(allow="app/"), callback="parse_item"),
 	)
 
 	def parse_item(self, response):
+		
 		item = scrapy.Item()
 		soup = BeautifulSoup(response.body, 'lxml')
 		
-		div = soup.find(id="")
+		div_element = soup.find('div', {'id': 'genresAndManufacturer'})
+		
+		
+		print (div_element)
+		print (div_element.find('b', string='Título:'))
+		
+		
+
+		titulo = div_element.find('b', string='Título:').next_sibling.strip()
+		
+		#Lista de generos
+		
+		genero_span = div_element.find('b', string='Género:').find_next('span')
+		genero_links = genero_span.find_all('a')
+		genero = [link.get_text(strip=True) for link in genero_links]
+			
+		#Lista de desarrolladores
+			
+		desarrollador_elements = div_element.find_all('b', string='Desarrollador:')
+		desarrolladores = [element.find_next('a').get_text(strip=True) for element in desarrollador_elements]
+		
+		#Lista de editores
+		
+		editor_elements = div_element.find_all('b', string='Editor:')
+		editores = [element.find_next('a').get_text(strip=True) for element in editor_elements]
+		
+		#Lista de franquicias
+
+		franquicia_elements = div_element.find_all('b', string='Franquicia:')
+		franquicias = [element.find_next('a').get_text(strip=True) for element in franquicia_elements]
+
+		#Fecha de lanzamiento
+		
+		fecha_lanzamiento_str = div_element.find('b', string='Fecha de lanzamiento:').next_sibling.strip()
+		fecha_lanzamiento = datetime.strptime(fecha_lanzamiento_str, '%d %b %Y')
+		
+		
+		#Elementos fuera del div
+		
+		price_element = soup.find('div', class_='game_purchase_price')
+		
+		discount_element = soup.find('div', class_='discount_original_price')
+		
+		#Descripcion
+		
+		descripcion = soup.find('div', class_="game_description_snippet").get_text(strip=True)
+		
+		#Imagenes
+		
+		imagen_cabecera = soup.find('img', class_='game_header_image_full')['src']
+		
+		images_list = soup.find('div', id= "highlight_strip_scroll").find_all('img')
+		
+		lista_imagenes = []
+		
+		for element in images_list:
+			lista_imagenes.append(element['src'])
+			
+				
+		if price_element:
+			precio = float(price_element.get_text(strip=True).replace(",", ".").replace("€", ""))
+		elif discount_element:
+			precio = float(discount_element.get_text(strip=True).replace(",", ".").replace("€", ""))
+		else:
+			precio = "-1"
+			
+			
+		item['Título']= titulo
+		item['Descripción']= descripcion
+		item['Precio']= precio
+		item['Desarrolladores']= desarrolladores
+		item['Editores']= editores
+		item['Franquicias']= franquicias
+		item['Fecha de lanzamiento']= fecha_lanzamiento
+		item['Imágen_Cabecera']= imagen_cabecera
+		item['Lista_Imagenes']= lista_imagenes
+		item['Url']= response.URL
+		
+		return item
